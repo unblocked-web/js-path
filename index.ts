e14142357ec5562137e85f7a182fde61036bec2a
Blake Byrnes <blakebyrnes@gmail.com>
2022-05-10 17:09:10 -0400

    feat: export all JsPathSteps

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
-import IJsPath, { IPathStep } from './interfaces/IJsPath';
+import IJsPath, { IPathStep, IMethod, INodeId, IPropertyName } from './interfaces/IJsPath';
 import IElementRect from './interfaces/IElementRect';
 import INodePointer from './interfaces/INodePointer';
 import IJsPathError from './interfaces/IJsPathError';
 import INodeVisibility from './interfaces/INodeVisibility';
 
-export { IJsPath, IPathStep, IElementRect, INodePointer, INodeVisibility, IJsPathError };
+export {
+  IJsPath,
+  IPathStep,
+  IElementRect,
+  INodePointer,
+  INodeVisibility,
+  IJsPathError,
+  IMethod,
+  INodeId,
+  IPropertyName,
+};
